096cd95237a98fee09a3939aca53707c5dd032dd
zachary zhuo <courage0322@gmail.com>
2019-11-20 01:57:47 +0800

    gh-page

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/zachary-page/',
 
     /**
      * Source Maps
